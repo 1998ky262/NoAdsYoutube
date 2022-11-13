5094a58e1d3ce981413cbb43788bc1c1026b3cd0
小惑星　 <106678831+1998ky262@users.noreply.github.com>
2022-11-13 20:47:16 +0900

    Create NoAdsYoutube.js

---
+++ NoAdsYoutube.js
@@ -0,0 +1,12 @@
+const a = getParam("v");
+// youtubeの動画idを取得
+location("https://1998ky262.github.io/NoAdsYoutube?v="+a);
+function getParam(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
