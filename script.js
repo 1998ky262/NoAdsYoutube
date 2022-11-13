0246d38de86e8128fa5789ff130c4a1272cc31a8
小惑星　 <106678831+1998ky262@users.noreply.github.com>
2022-11-13 21:20:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 const a = getParam("v");
 // youtubeの動画idを取得
-if(!a){
-  alert("なんかおかしいんちゃうかぁ？");
-}
-else{
 const b=document.getElementByID("video");
 b.innerHTML=`<iframe width="853" height="480" src="https://www.youtube.com/embed/${a}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>`;
-}
 function getParam(name, url) {
   if (!url) url = window.location.href;
   name = name.replace(/[\[\]]/g, "\\$&");
